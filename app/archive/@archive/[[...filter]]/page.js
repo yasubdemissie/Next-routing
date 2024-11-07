23f8f8096961ba11ec6f40a1a11e1bb70a972df8
yasubdemissie <yasdam777@gmail.com>
2024-11-07 10:14:58 +0300

    nav of month and year

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,21 +1,58 @@
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import NewsList from "@/Components/NewsList";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
 import Link from "next/link";
 
-function YearPage({ }) {
-  const news = getAvailableNewsYears();
+function YearPage({ params }) {
+  const newsYear = getAvailableNewsYears();
+  let news, months;
+
+  const selectedYear = params?.filter?.[0];
+  const selectedMonth = params?.filter?.[1];
+
+  news = <p>No News Found</p>;
+
+  if (selectedYear && !selectedMonth) {
+    const newsForYear = getNewsForYear(selectedYear);
+    months = getAvailableNewsMonths(selectedYear);
+    news = <NewsList news={newsForYear} />;
+  }
+  if (selectedYear && selectedMonth) {
+    const newsOfMonth = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    news = <NewsList news={newsOfMonth} />;
+  }
 
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {news.map((year) => (
-            <li key={year}>
-              <Link href={`/archive/${year}`}>{year}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          {selectedYear === undefined ? (
+            <ul>
+              {newsYear.map((year) => (
+                <li key={year}>
+                  <Link href={`/archive/${year}`}>{year}</Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <ul>
+              {months?.map((month) => (
+                <li key={month}>
+                  <Link href={`/archive/${selectedYear}/${month}`}>
+                    {month}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </nav>
+      </header>
+      {news}
+    </>
   );
 }
 
