92431f68e231f34f03ae63ecb1fda8fd4ea6758d
yasubdemissie <yasdam777@gmail.com>
2024-11-07 09:32:48 +0300

    fiter done

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,14 +1,14 @@
-import { getAvailableNewsYears } from "@/lib/news";
+import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
 import Link from "next/link";
 
-function Archive() {
-  const newsYear = getAvailableNewsYears();
+function YearPage({ }) {
+  const news = getAvailableNewsYears();
 
   return (
     <header id="archive-header">
       <nav>
         <ul>
-          {newsYear.map((year) => (
+          {news.map((year) => (
             <li key={year}>
               <Link href={`/archive/${year}`}>{year}</Link>
             </li>
@@ -19,4 +19,4 @@ function Archive() {
   );
 }
 
-export default Archive;
+export default YearPage;
